547c93e3410e6afe655a6e3da2a1508665ed164f
Ches Romakhin <chesromakhin@gmail.com>
2018-06-12 13:24:16 +0300

    Start frontend

--- crossgress-react-ui/src/App.js
+++ crossgress-react-ui/src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Label, Icon} from 'semantic-ui-react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+const timesRegExp = /x(\d+(-\d+)?)/;
 
 class App extends Component {
+
+  state = {
+    value: ''
+  };
+
   render() {
+    let tags = [];
+    if (this.state.value.endsWith(' ') && timesRegExp.test(this.state.value.substr(0, this.state.value.length - 1))) {
+      let times = timesRegExp.exec(this.state.value.substr(0, this.state.value.length - 1))[1];
+
+      tags.push((
+        <Label key='1'><Icon name='redo'/>{times}</Label>
+      ));
+    }
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {tags}
+        <input onChange={(event) => this.setState({value: event.target.value})}/>
       </div>
     );
   }
